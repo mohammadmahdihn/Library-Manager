9473fce7ec008bbdf679357abacf67c30dbd9c1c
mohammadmahdihn <mohammadmahdiheydarinasab@gmail.com>
2023-07-28 00:11:05 +0330

    Add some models and serializers

--- library/models.py
+++ library/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -6,4 +7,14 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    available = models.BooleanField()
+    publisher = models.CharField(max_length=200)
+    publish_date = models.DateField(null=True)
+    is_available = models.BooleanField(default=True)
+
+
+class BorrowLog(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    is_returned = models.BooleanField(default=False)
